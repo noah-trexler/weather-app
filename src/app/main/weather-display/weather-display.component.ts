af30cfa504ea59b3b0b4af97b2b8e8206216cfa1
Noah Trexler 2380503 <noah.trexler@cvshealth.com>
2023-08-18 14:08:45 -0400

    full functionality

--- src/app/main/weather-display/weather-display.component.ts
+++ src/app/main/weather-display/weather-display.component.ts
@@ -1,20 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { WeatherService } from 'src/app/shared/weather-service.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { simpleForecast } from 'src/app/shared/models/forecast.model';
+import { WeatherService } from 'src/app/shared/services/weather-service.service';
 
 @Component({
   selector: 'app-weather-display',
   templateUrl: './weather-display.component.html',
   styleUrls: ['./weather-display.component.css'],
 })
-export class WeatherDisplayComponent {
-  @Input() coords?: string;
-  forecast?: any;
+export class WeatherDisplayComponent implements OnInit {
+  @Input() coords: string = '33.7488,-84.3877';
+  forecast?: simpleForecast[];
+  forecastSubscription!: Subscription;
 
   constructor(private weatherService: WeatherService) {}
 
-  onSubmit(input: string) {
-    const _input = input.split(',');
-    this.weatherService.getForecast(+_input[0], +_input[1]);
+  onSubmit() {
+    // const _input = this.coords.split(',');
+    // this.weatherService.getForecast(+_input[0], +_input[1]).subscribe((res) => {
+    //   console.log(res);
+    //   this.forecast = res;
+    // });
+    this.weatherService.getForecastFromLocation();
+  }
+
+  ngOnInit(): void {
+    this.forecastSubscription = this.weatherService.forecastData.subscribe(
+      (forecast) => {
+        this.forecast = forecast;
+      }
+    );
   }
 }
---
+++ src/app/shared/models/forecast.model.ts
@@ -0,0 +1,34 @@
+export interface forecast {
+  number: number;
+  name: string;
+  startTime: string;
+  endTime: string;
+  isDaytime: boolean;
+  temperature: number;
+  temperatureUnit: string;
+  temperatureTrend: string | null;
+  probabilityOfPrecipitation: {
+    unitCode: string;
+    value: number;
+  };
+  dewpoint: {
+    unitCode: string;
+    value: number;
+  };
+  relativeHumidity: {
+    unitCode: string;
+    value: number;
+  };
+  windSpeed: string;
+  windDirection: string;
+  icon: string;
+  shortForecast: string;
+  detailedForecast: string;
+}
+
+export interface simpleForecast {
+  isDaytime: boolean;
+  temp: number;
+  humidity: number;
+  precip: number;
+}
