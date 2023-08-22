3ca2ac0486195b89b93a8f5e81c37d0a9401d4b7
Noah Trexler 2380503 <noah.trexler@cvshealth.com>
2023-08-22 14:18:21 -0400

    forecast get req

---
+++ api/routes/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+var got = require("got");
+
+require("dotenv").config();
+
+/* GET forecast. */
+router.get("/forecast", function (req, res, next) {
+  var location = "";
+  got(`https://api.weather.gov/points/${req.query.lat},${req.query.lon}`)
+    .json()
+    .then((responseData) => {
+      location = responseData.properties.relativeLocation.properties;
+      return got(responseData.properties.forecast).json();
+    })
+    .then((responseData) => {
+      res.status(200).json({
+        message: "Successfully retrieved forecast data",
+        locationData: location,
+        forecastData: responseData,
+      });
+    })
+    .catch((err) => console.error(err));
+});
+
+module.exports = router;
